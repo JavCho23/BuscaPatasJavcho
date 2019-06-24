440032019d798427a44583a5563447550b8a6818
JavCho23 <javier120699lili@gmail.com>
2019-06-23 20:25:43 -0500

    login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,36 +1,67 @@
 <template>
-    <v-form>
+    <v-form @submit.prevent="login">
         <v-container grid-list-md  >
             <v-layout row wrap>
-                <v-flex xs12>
+                <v-flex xs12 text-xs-center>
                     <h1>Login</h1>
                 </v-flex>
             </v-layout>
-            <v-layout  fill-height column align-center   >
-                <v-flex xs3>
+            <v-layout  row wrap justify-center  >
+                <v-flex xs6>
                     <v-text-field
-                color="secondary"
-                label="Usuario"
-                placeholder="javCho23"
-                v-model="usuario"
-            >
-            </v-text-field>
-                </v-flex>
-                <v-flex xs3>
-                    <v-text-field
-                color="secondary"
-                label="Contraseña"
-                placeholder="************"
-                type="password"
-                v-model="pass"
-            >
-            </v-text-field>
-                </v-flex>
-                <v-flex xs1>
-                    <v-btn color="info">Ingresar</v-btn>
+                        color="secondary"
+                        label="Usuario"
+                        placeholder="javCho23"
+                        type="email"
+                        v-model="user"
+                    >
+                    </v-text-field>
                 </v-flex>
             </v-layout>
+            <v-layout row wrap justify-center>
+                    <v-flex xs6>
+                    <v-text-field
+                        color="secondary"
+                        label="Contraseña"
+                        placeholder="************"
+                        type="password"
+                        v-model="pass"
+                    >
+                    </v-text-field>
+                </v-flex>
+            </v-layout>
+                
+               
             
+            <v-layout row wrap justify-center>
+                <v-flex xs3>
+                            <v-text-field
+                            color="secondary"
+                            type="submit"
+                        >
+                    </v-text-field>
+                </v-flex>
+            </v-layout>
         </v-container>
     </v-form>
 </template>
+<script>
+import firebase from 'firebase/app'
+import 'firebase/auth'
+export default {
+    data(){
+        return{
+            user: '',
+            pass: ''
+        }
+    },
+    methods:{
+        login(){
+           firebase.auth()
+           .signInWithEmailAndPassword(this.user, this.pass) // eslint-disable-next-line
+           .then((user) => alert('Usuario Conectado'), (error) => console.error(error)) 
+           
+        }
+    }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
 
